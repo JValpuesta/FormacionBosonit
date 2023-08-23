cde6a8a200e9a7fbb8d16ef835cef0dd0b65657f
JValpuesta <valpuestajorge@gmail.com>
2023-08-23 17:32:52 +0200

    23/08

--- block16-spring-cloud/trip-backend/src/main/java/com/bosonit/tripbackend/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bosonit.tripbackend.repository;
-
-import com.bosonit.tripbackend.domain.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
-}
