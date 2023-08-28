8bf3beba9fa50aa696a33e58ca2eb9e0fead146f
JValpuesta <61006206+JValpuesta@users.noreply.github.com>
2023-08-28 11:10:59 +0200

    Delete trip-frontend directory

--- trip-frontend/src/main/java/com/bosonit/tripfrontend/repository/TicketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bosonit.tripfrontend.repository;
-
-import com.bosonit.tripfrontend.domain.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Integer> {
-}
